0d0a01e0f7c09a64956968699b5788d93dd28cb1
Sergio <sergioarturo.gallo@e-campus.uab.cat>
2020-11-10 21:13:24 +0100

    arreglo main

--- src/mvc/controller/Main.java
+++ src/mvc/controller/Main.java
@@ -1,5 +1,6 @@
 package mvc.controller;
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import mvc.model.Puntuaciones;
@@ -8,112 +9,150 @@ import mvc.view.Partida;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		int opcion = 0, posX = 0, posY = 0;
-		boolean posValida = false;
+		int opcion = 0, posX = 0, posY = 0, seguir = 1;
+		boolean posValida = false, entrada = true;
+		String salir;
 		
 		Partida partida = new Partida();
 		Scanner sc = new Scanner(System.in);
-		
-		while (opcion < 1 || opcion > 5){
-			System.out.println("Selecciona un nivel:");
-			System.out.println("Pulsa 1: Nivel Facil");
-			System.out.println("Pulsa 2: Nivel Medio");
-			System.out.println("Pulsa 3: Nivel Dificil");
-			System.out.println("Pulsa 4: Nivel Muy Dificil");
-			System.out.println("Pulsa 5: Salir");
-			opcion = sc.nextInt();
-		}
-
-		switch(opcion) {
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				partida.setNivel(opcion);
-				partida.mostrarTablero();
-				break;
-			case 5:
-				System.out.println("�Vuelve pronto!");
-				sc.close();	
-				break;
-			default:
-				break;
-		}
-		
-		while (partida.getEnJuego()) {
-			opcion = 0;
-			while (opcion < 1 || opcion > 4){
-				System.out.println("Que quieres hacer? :");
-				System.out.println("Pulsa 1: Marcar casilla");
-				System.out.println("Pulsa 2: Desmarcar casilla");
-				System.out.println("Pulsa 3: Destapar casilla");
-				System.out.println("Pulsa 4: Salir");
-				opcion = sc.nextInt();
-			}
-			if(opcion>0 || opcion <5) {
-				switch(opcion) {
-					case 1: 
-						while(!posValida) {
-							System.out.println("Elige una fila:");
-							posX = sc.nextInt();
-							System.out.println("Elige una columna:");
-							posY = sc.nextInt();
-							posValida = partida.posCorrecta(posX-1, posY-1);
-							if(!posValida) {
-								System.out.println("Numeros incorrectos\n");
+		Scanner reader = new Scanner(System.in);
+		while(entrada) {
+			try {
+				while(seguir == 1) {
+					while (opcion < 1 || opcion > 5){
+						System.out.println("Selecciona un nivel:");
+						System.out.println("Pulsa 1: Nivel Facil");
+						System.out.println("Pulsa 2: Nivel Medio");
+						System.out.println("Pulsa 3: Nivel Dificil");
+						System.out.println("Pulsa 4: Nivel Muy Dificil");
+						System.out.println("Pulsa 5: Salir");
+						opcion = sc.nextInt();
+					}
+					
+					switch(opcion) {
+						case 1:
+						case 2:
+						case 3:
+						case 4:
+							partida.setNivel(opcion);
+							partida.mostrarTablero();
+							seguir = 0;
+							break;
+						case 5:
+							System.out.println("Seguro que quieres salir? (S/N)\n");
+							salir = reader.next();
+							seguir = partida.continuarJuego(salir);
+							if(seguir == 0) {
+								System.out.println("Vuelve pronto!");
+								partida.setEnJuego(false);
+								entrada=false;
+								sc.close();	
+							}else {
+								opcion = 0;
+								seguir = 1;
 							}
-						}
-						partida.marcarCasilla(posX-1, posY-1);
-						break;
-					case 2:
-						while(!posValida) {
-							System.out.println("Elige una fila:\n");
-							posX = sc.nextInt();
-							System.out.println("Elige una columna:\n");
-							posY = sc.nextInt();
-							posValida = partida.posCorrecta(posX-1, posY-1);
-							if(!posValida) {
-								System.out.println("Numeros incorrectos");
-							}
-						}
-						partida.desmarcarCasilla(posX-1, posY-1);
-						break;
-					case 3:
-						while(!posValida) {
-							System.out.println("Elige una fila:");
-							posX = sc.nextInt();
-							System.out.println("Elige una columna:");
-							posY = sc.nextInt();
-							posValida = partida.posCorrecta(posX-1, posY-1);
-							if(!posValida) {
-								System.out.println("Numeros incorrectos");
-							}
-						}
-						partida.destaparCasilla(posX-1, posY-1);
-						if(!partida.getEnJuego()) {
-							System.out.println("------------");
-							System.out.println("Has perdido!");
-							System.out.println("------------");
-						}
-						break;
-					case 4: 
-						partida.setEnJuego(false);
-						break;
+							
+							break;
+						default:
+							break;
+					}
 				}
-				
-				partida.mostrarTablero();
-				posValida = false;
+				while (partida.getEnJuego()) {
+					try {
+						opcion = 0;
+						while (opcion < 1 || opcion > 4){
+							System.out.println("Que quieres hacer? :");
+							System.out.println("Pulsa 1: Marcar casilla");
+							System.out.println("Pulsa 2: Desmarcar casilla");
+							System.out.println("Pulsa 3: Destapar casilla");
+							System.out.println("Pulsa 4: Salir");
+							opcion = sc.nextInt();
+						}
+						if(opcion>0 || opcion <5) {
+							switch(opcion) {
+								case 1: 
+									while(!posValida) {
+										System.out.println("Elige una fila:");
+										posX = sc.nextInt();
+										System.out.println("Elige una columna:");
+										posY = sc.nextInt();
+										posValida = partida.posCorrecta(posX-1, posY-1);
+										if(!posValida) {
+											System.out.println("Numeros incorrectos\n");
+										}
+									}
+									partida.marcarCasilla(posX-1, posY-1);
+									break;
+								case 2:
+									while(!posValida) {
+										System.out.println("Elige una fila:\n");
+										posX = sc.nextInt();
+										System.out.println("Elige una columna:\n");
+										posY = sc.nextInt();
+										posValida = partida.posCorrecta(posX-1, posY-1);
+										if(!posValida) {
+											System.out.println("Numeros incorrectos");
+										}
+									}
+									partida.desmarcarCasilla(posX-1, posY-1);
+									break;
+								case 3:
+									while(!posValida) {
+										System.out.println("Elige una fila:");
+										posX = sc.nextInt();
+										System.out.println("Elige una columna:");
+										posY = sc.nextInt();
+										posValida = partida.posCorrecta(posX-1, posY-1);
+										if(!posValida) {
+											System.out.println("Numeros incorrectos");
+										}
+									}
+									partida.destaparCasilla(posX-1, posY-1);
+									if(!partida.getEnJuego()) {
+										System.out.println("------------");
+										System.out.println("Has perdido!");
+										System.out.println("------------");
+									}
+									break;
+								case 4:
+									System.out.println("Seguro que quieres salir? (S/N)\n");
+									salir = reader.next();
+									seguir = partida.continuarJuego(salir);
+									if(seguir == 0) {
+										partida.setEnJuego(false);
+										entrada=false;
+									}
+									break;
+							}
+							
+							partida.mostrarTablero();
+							posValida = false;
+						}
+						
+						if(!partida.getEnJuego()) {
+							System.out.println("Nom del jugador?\n");
+							int punts=partida.getPuntuacion();
+							String nom = null; 
+							nom = sc.next();
+							Puntuaciones puntuacion = new Puntuaciones(nom, punts, partida.getNivel());
+							//escrivim puntuacions i mostrem per nivell
+							puntuacion.escribirPuntuaciones(punts,partida.getNivel(),nom);
+							puntuacion.mostrar_Puntuaciones(partida.getNivel());
+						}
+					}catch(InputMismatchException ex) {
+						System.out.println("Error: Introduce un caracter valido");
+						System.out.println();
+						sc.next();
+					}
+
+				}
+			}catch(InputMismatchException ex) {
+				System.out.println("Error: Introduce un caracter valido");
+				System.out.println();
+				sc.next();
 			}
-			
 		}
-		System.out.println("Nom del jugador?\n");
-		int punts=partida.getPuntuacion();
-		String nom = null; 
-		nom = sc.next();
-		Puntuaciones puntuacion = new Puntuaciones(nom, punts, partida.getNivel());
-		//escrivim puntuacions i mostrem per nivell
-		puntuacion.escribirPuntuaciones(punts,partida.getNivel(),nom);
-		puntuacion.mostrar_Puntuaciones(partida.getNivel());
+		
 		sc.close();
 	}
 	
